6f9c794b0b78581219fade1fbc2fb87455cb9dad
Lucas Gomes <lucasgomescefetmg@gmail.com>
2026-06-14 02:07:49 -0300

    Mais coisas ae

--- src/config.py
+++ src/config.py
@@ -3,11 +3,4 @@ LARGURA_TELA = 1280
 ALTURA_TELA  = 720
 FPS          = 60
 TITULO_JANELA  = "Flappy Bird PyGame"
- 
-# Cores prontas para a fazer a ideia inicial do jogo
-AZUL_CEU = (78, 192, 202)
-BRANCO = (255, 255, 255)
-PRETO = (0, 0, 0)
-CINZA = (212,212,212)
-
 CAMINHO_RECORDE = "data/recorde.txt"
